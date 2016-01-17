d35e7db8a633d53d4b556c0b65788c8559575bbb
Christian Tobias <cwtobias6@gmail.com>
2016-01-17 09:58:07 -0600

    added selected function to hard,easy

--- public/colorGame.js
+++ public/colorGame.js
@@ -5,6 +5,35 @@ var colorDisplay = document.getElementById("colorDisplay");
 colorDisplay.textContent = pickedColor;
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+
+easyBtn.addEventListener("click", function() {
+	hardBtn.classList.remove("selected");
+	easyBtn.classList.add("selected");
+
+});
+
+hardBtn.addEventListener("click", function() {
+	easyBtn.classList.remove("selected");
+	hardBtn.classList.add("selected");
+
+});
+
+//resets game if reset button is clicked
+resetButton.addEventListener("click", function() {
+	colors = generateRandomColors(6);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.background = colors[i];
+	}
+
+	h1.style.background = "#232323";
+
+});
 
 for (var i = 0; i < squares.length; i++) {
 
@@ -20,6 +49,7 @@ for (var i = 0; i < squares.length; i++) {
 		//compare clicked color vs. picked color
 		if (clickedColor === pickedColor) {
 			messageDisplay.textContent = "Correct!";
+			resetButton.textContent = "Play Again?"
 			changeColors(clickedColor);
 			h1.style.background = clickedColor;
 			
@@ -48,15 +78,10 @@ function pickColor() {
 }
 
 function generateRandomColors(number) {
-
 	var randomColorsArray = [];
-
-
 	for (var i = 0; i < number; i++) {
-
 		randomColorsArray.push(randomColor());
 	};
-
 
 	return randomColorsArray;
 
